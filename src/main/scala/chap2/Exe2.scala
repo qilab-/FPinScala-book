ae3f0e8c5317601c669422e925d9a4e29e0acfba
qilab gamma <qilab@gmx.com>
2015-08-01 00:54:20 +0900

    recursive

--- src/main/scala/chap2/Exe2.scala
+++ src/main/scala/chap2/Exe2.scala
@@ -8,5 +8,21 @@ object Exe2 {
     }
   }
 
+  def isSorted2[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    as.zip(as.tail).forall(ordered.tupled)
+  }
+
+  def isSortedRec[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int, acc: Boolean): Boolean = {
+      if (n <= 0)
+        acc
+      else
+        loop(n - 1, acc && ordered(as(n - 1), as(n)))
+    }
+
+    loop(as.length - 1, true)
+  }
+
 }
 
