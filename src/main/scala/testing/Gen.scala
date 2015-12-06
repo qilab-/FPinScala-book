d37a2382365bd4e1eac0a1ea39cb57dd0e5b411b
qilab- <qilab@gmx.com>
2015-12-06 18:26:14 +0900

    Exercise 8.4

--- src/main/scala/testing/Gen.scala
+++ src/main/scala/testing/Gen.scala
@@ -1,5 +1,7 @@
 package testing
 
+import state.{RNG, State}
+
 trait Prop {
   def check: Boolean
 
@@ -8,5 +10,24 @@ trait Prop {
     override def check: Boolean = {
       if (this.check) p.check else false
     }
+  }
+}
+
+case class Gen[A](sample: State[RNG, A])
+
+object Gen {
+  // Exercise 8.4
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    val (s, e) =
+      if (start < stopExclusive)
+        (start, stopExclusive)
+      else
+        (stopExclusive, start)
+    val run: RNG => (Int, RNG) = {
+      val rand = RNG.nonNegativeLessThan(e - s)
+      RNG.map(rand)(_ + s)
+    }
+    val state = State[RNG, Int](run)
+    Gen(state)
   }
 }
